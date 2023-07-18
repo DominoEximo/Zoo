0ef9e5e8e5e0a08269641968fbab0d959270fcf7
PappDávid <PappDavid32@gmail.com>
2023-07-18 21:41:29 +0200

    Created StoreZooTest

---
+++ Zoo/src/test/java/StoreZooTest.java
@@ -0,0 +1,14 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class StoreZooTest {
+
+	@Test
+	void saveZooTest() {
+		fail("Not yet implemented");
+	}
+	
+	
+
+}
