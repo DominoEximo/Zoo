b4a1929093359c9701cca95f8d601add0077b1ca
PappDávid <PappDavid32@gmail.com>
2023-07-28 15:41:25 +0200

    Extended zooTest

--- Zoo/src/test/java/hu/neuron/mentoring/zoo/ZooTest.java
+++ Zoo/src/test/java/hu/neuron/mentoring/zoo/ZooTest.java
@@ -6,15 +6,30 @@ import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
 
 public class ZooTest {
-	
+
 	Zoo zoo = new Zoo();
 
 	@Test
 	public void reserveTest() {
-		
+
 		zoo.reserve(new Reservation(null, null, null, null, null, null));
-		
+
 		Assertions.assertEquals(1, zoo.getReservations().size());
+	}
+
+	@Test
+	public void flushReservationsTest() {
+		zoo.flushReservations();
+
+		Assertions.assertEquals(0, zoo.getReservations().size());
+	}
+
+	@Test
+	public void addEmployeeTest() {
+		zoo.addEmployee(null);
+
+		Assertions.assertEquals(1, zoo.getEployees().size());
+
 	}
 
 }
